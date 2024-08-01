d0ff4837a83a0c8077608429f80885a261d99e69
OUAEL <benamaraouael1@gmail.com>
2024-08-01 14:24:09 +0100

    display fix

--- components/layout/RightVideoSection/index.jsx
+++ components/layout/RightVideoSection/index.jsx
@@ -107,12 +107,13 @@ function RightVideoSection({
           <video
             width="100%"
             height="100%"
-            className="h-full w-full object-cover rounded-[20px]"
+            className="z-30 h-full w-full object-cover rounded-[20px]"
             controls
           >
             <source src={videoLink} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+
           <a
             target="_blank"
             href={docLink}
